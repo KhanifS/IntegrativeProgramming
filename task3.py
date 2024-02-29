853f8dff0fa2715b7f131833fa66c962af984a65
unknown <khanif.sbt7252@gmail.com>
2024-02-29 14:11:14 +0700

    update

---
+++ task3.py
@@ -0,0 +1,9 @@
+import math
+
+number = int(input("Enter a number: "))
+if number < 100:
+    print("Small")
+elif number > 200:
+    print("Large")
+else:
+    print("Medium")
---
+++ task4.py
@@ -0,0 +1,5 @@
+import math
+
+number = int(input("Enter a number: "))
+sum_of_values = (number * (number + 1)) // 2
+print("Sum of values:", sum_of_values)
